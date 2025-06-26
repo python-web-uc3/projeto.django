d84f900ffa1f90f7d435541410ffba7bd4eb9eea
felipefvalle <felipefranck0@gmail.com>
2025-06-25 21:51:20 -0300

    Aula 8 - Django com Templates

---
+++ primeiro_app/urls.py
@@ -0,0 +1,8 @@
+# minha_app/urls.py
+from django.urls import path
+from . import views # <-- Importa as views do arquivo views.py desta aplicação
+
+urlpatterns = [
+    path('', views.home_view, name='home'), # Mapeia a URL raiz desta aplicação ('' ) para a função home_view
+    path('login/', views.login),
+]
--- primeiro_app/views.py
+++ primeiro_app/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 
 def home(request):
     return HttpResponse ('Olá, Django!')
+
+def home_view(request):
+    """
+    Renderiza a página inicial com um template HTML, passando a data e hora atuais como contexto.
+    """
+    context = {
+        'data_e_hora': datetime.now() # Variável Python que será acessível no template HTML
+    }
+    return render(request, 'meu_primeiro_html.html', context) # Renderiza o template especificado
+
+def login(request):        
+    usuario = 'Felipe'
+    senha = '1234'
+    if usuario == 'Felipe' and senha == '1234':
+        resposta = "Login feito com sucesso!"
+    else:
+        resposta = "Acesso Negado - Tente Novamente"
+    return HttpResponse(resposta)
